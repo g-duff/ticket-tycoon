d0e29c9f04069875d8f9cbb79ded51e36f1dbe6d
George <george.duffett@icloud.com>
2026-07-17 18:11:56 +0100

    chore: init node project

---
+++ esbuild.config.mjs
@@ -0,0 +1,18 @@
+import * as esbuild from "esbuild";
+import { exit } from "node:process";
+
+esbuild
+  .build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    outdir: 'dist',
+    minify: false,
+    format: "esm",
+    platform: "node",
+    target: "node24",
+    // Fix for https://github.com/evanw/esbuild/pull/2067
+    banner: {
+      js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
+    }
+  })
+  .catch(() => exit(1));
